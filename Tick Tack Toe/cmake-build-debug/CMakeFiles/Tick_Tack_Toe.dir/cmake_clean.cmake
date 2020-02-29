a2496b86e0687f2197840e1d84924a5e1d112bbd
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-02-29 20:53:39 +0500

    created

---
+++ Tick Tack Toe/cmake-build-debug/CMakeFiles/Tick_Tack_Toe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tick_Tack_Toe.dir/main.cpp.obj"
+  "Tick_Tack_Toe.exe"
+  "Tick_Tack_Toe.exe.manifest"
+  "Tick_Tack_Toe.pdb"
+  "libTick_Tack_Toe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tick_Tack_Toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tick Tack Toe/cmake-build-debug/CMakeFiles/Tick_Tack_Toe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tick_Tack_Toe.
+# This may be replaced when dependencies are built.
