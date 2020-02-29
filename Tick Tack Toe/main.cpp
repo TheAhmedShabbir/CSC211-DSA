0a914f00ea147d66c3ba6937e83c16887be24aa8
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-03-01 01:14:08 +0500

    x

--- Tick Tack Toe/main.cpp
+++ Tick Tack Toe/main.cpp
@@ -2,61 +2,75 @@
 using namespace std;
 
 int main() {
-    char array[3][3] = {},choice;
+    char array[9] = {},choice;
     int box;
 
+    do {
 //choose symbol
-    cout << "Choose your Symbol 'x' and 'o': \n";
-    cin >> choice;
-
+        cout << "Choose your Symbol 'x' and 'o': \n";
+        cin >> choice;
 //position
+        if (choice == 'x') {
+            do {
+            cout << "Enter in which box you want to put sign: \n";
+            cout << "boxes: 1st 2nd 3rd 4th 5th 6th 7th 8th 9th\n";
+            cin >> box;
 
-    if (choice == 'x' || choice == 'o') {
-        cout << "Enter in which box you want to put sign: \n";
-        cout << "boxes: 1st 2nd 3rd 4th 5th 6th 7th 8th 9th\n";
-        cin >> box;
-
-        if (box == 1) {
-            array[0][0] = 'x';
+            if (box == 1) {
+                array[0] = 'x';
+            }
+            if (box == 2) {
+                array[1] = 'x';
+            }
+            if (box == 3) {
+                array[2] = 'x';
+            }
+            if (box == 4) {
+                array[3] = 'x';
+            }
+            if (box == 5) {
+                array[4] = 'x';
+            }
+            if (box == 6) {
+                array[5] = 'x';
+            }
+            if (box == 7) {
+                array[6] = 'x';
+            }
+            if (box == 8) {
+                array[7] = 'x';
+            }
+            if (box == 9) {
+                array[8] = 'x';
+            }
+            cout << array[0] << "     |" << array[1] << "     |" << array[2];
+            cout << "\n______";
+            cout << "|______";
+            cout << "|______\n";
+            cout << array[3] << "     |" << array[4] << "     |" << array[5];
+            cout << "\n______";
+            cout << "|______";
+            cout << "|______\n";
+            cout << array[6] << "     |" << array[7] << "     |" << array[8] << "\n";
+            }
+            while (box != 9);
+            //result
+            if (array[0] == array[1] && array[1] == array[2]){
+                cout << "x wins";
+                break;
+            }
+            if (array[3] == array[4] && array[4] == array[5]){
+                cout << "x wins";
+                break;
+            }
+            if (array[6] == array[7] && array[7] == array[8]){
+                cout << "x wins";
+                break;
+            }
         }
-        if (box == 2) {
-            array[0][1] = 'x';
-        }
-        if (box == 3) {
-            array[0][2] = 'x';
-        }
-        if (box == 4) {
-            array[1][0] = 'x';
-        }
-        if (box == 5) {
-            array[1][1] = 'x';
-        }
-        if (box == 6) {
-            array[1][2] = 'x';
-        }
-        if (box == 7) {
-            array[2][0] = 'x';
-        }
-        if (box == 8) {
-            array[2][1] = 'x';
-        }
-        if (box == 9) {
-            array[2][2] = 'x';
-        }
+        else
+            cout << "Invalid input..\nPlease try again!\n";
     }
-
-//_____board______
-
-    cout << array[0][0] << "     |" << array[0][1] << "     |" << array[0][2];
-    cout << "\n______";
-    cout << "|______";
-    cout << "|______\n";
-    cout << array[1][0] << "     |" << array[1][1] << "     |" << array[1][2];
-    cout << "\n______";
-    cout << "|______";
-    cout << "|______\n";
-
-    cout << array[2][0] << "     |" << array[2][1] << "     |" << array[2][2] << "\n";
-
+    while (choice != 'x' && choice != 'o');
     return 0;
 }
