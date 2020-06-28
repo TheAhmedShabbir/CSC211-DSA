cac34a79e7bad2f8cf19d3cc1d5ed9c28bf73e39
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-06-28 12:28:13 +0500

    new

---
+++ Airport/cmake-build-debug/CMakeFiles/Airport.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Airport.exe"
+  "Airport.exe.manifest"
+  "Airport.pdb"
+  "CMakeFiles/Airport.dir/main.cpp.obj"
+  "libAirport.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Airport.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
