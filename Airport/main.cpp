17642218a0564f89225885f1b00f2d3e8d80666c
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-06-30 16:54:43 +0500

    heyy

--- Airport/main.cpp
+++ Airport/main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
-    cout << "airport program";
+    cout << "--------MENU---------\n"
+            "Press 1 for Departure\n"
+            "Press 2 for Arrival\n"
+            "Press 3 for Emergency\n"
+            "Press 4 for Reports\n";
     return 0;
 }
