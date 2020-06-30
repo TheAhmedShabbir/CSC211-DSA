9b016fd6e3fcf57d18085e917b4c79e8fbde42e2
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-06-30 18:07:11 +0500

    ese hi bs

--- Airport/ArrivalQueue.cpp
+++ Airport/ArrivalQueue.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+const int size = 10;
 class ArrivalQueue {
-
 public:
+    int counter;
+    int array[size] = {};
     void Enqueue(int num) {
+        for(counter = 0; counter > size; counter++){
 
+        }
     }
+
+    bool isFull() {
+        if (counter >= size){
+            return true;
+        }
+        else
+            return false;
+    };
 };
