ddd427e0336866131f224b6b0ee5e29b7070609f
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-07-16 04:52:56 +0500

    hola

---
+++ ExpressionEvaluation/stack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#define size 30
+using namespace std;
+
+class stack {
+private:
+    int top,counter;
+    char array[size];
+public:
+    void isFull(){
+
+    }
+    void isEmpty(){
+
+    }
+    void push(){
+
+    }
+    void pop(){
+
+    }
+};
