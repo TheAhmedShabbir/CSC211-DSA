01452d7878a836fe5c96115f739fccf529eaec4c
Ahmed Shabbir <ahmed.shabbir1308@gmail.com>
2020-07-25 16:03:25 +0500

    Add files via upload

---
+++ File Management System/cmake-build-debug/CMakeFiles/File_Management_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File_Management_System.dir/main.cpp.obj"
+  "File_Management_System.exe"
+  "File_Management_System.exe.manifest"
+  "File_Management_System.pdb"
+  "libFile_Management_System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/File_Management_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ File Management System/cmake-build-debug/CMakeFiles/File_Management_System.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for File_Management_System.
+# This may be replaced when dependencies are built.
