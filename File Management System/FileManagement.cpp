94445b1bca4803bc887f9db83f947d74f7ccd31b
oyeahmed <ahmed.shabbir1308@gmail.com>
2020-07-27 15:59:34 +0500

    createFile not working

--- File Management System/FileManagement.cpp
+++ File Management System/FileManagement.cpp
@@ -25,14 +25,20 @@ public:
         for(counter = 0; counter <= totalFiles; counter++){
             fileInfo[counter].fileIndex = -1;
         }
-        fileInfo[4].start = -3;
+        fileInfo[4].fileIndex = -3;
 
         for (counter = 0; counter <= 100; counter++) {
             fileData[counter].nextIndex = -1;
         }
         fileData[99].nextIndex = -3;
 
-
+        if(fileInfo[counter].fileIndex == -1 || fileData[counter].nextIndex == -1){
+            fileInfo[counter].fileName = FileName[counter];
+            fileInfo[counter].fileExt = ext[counter];
+            fileData[counter].Data = FileData[counter];
+            fileInfo[counter].start = counter;
+            fileInfo[counter].fileIndex = fileInfo[counter+1].fileIndex;
+        }
     }
 
     void deleteFile(char FileName[],char ext[]){
@@ -48,7 +54,7 @@ public:
         if(FileName[counter] != fileInfo->fileName || ext[counter] != fileInfo->fileExt) {
             cout << "File not Exist!!\n";
         } else
-            cout << fileData->Data;
+            cout << fileInfo[counter].start;
     }
 };
 
--- File Management System/main.cpp
+++ File Management System/main.cpp
@@ -6,32 +6,45 @@ using namespace std;
 
 int main() {
     int choice;
-    char fName[Size],fExt[4];
+    char fName[Size],fExt[4],fData[100];
     FileManagement File{};
+    do {
+            cout << "------Menu-----\n"
+                    "Press 1 to create file\n"
+                    "Press 2 to delete file\n"
+                    "Press 3 to view file\n"
+                    "Press 4 to Exit\n";
+            cin >> choice;
 
-    cout << "------Menu-----\n"
-            "Press 1 to create file\n"
-            "Press 2 to delete file\n"
-            "Press 3 to view file\n";
-    cin >> choice;
+            if (choice == 1){
+                cout << "Enter File Name \n";
+                cin >> fName;
+                cout << "Enter File extension \n";
+                cin >> fExt;
+                cout << "Enter File Data \n";
+                cin >> fData;
 
-    if(choice == 2){
-        cout << "Enter File Name \n";
-        cin >> fName;
-        cout << "Enter File extention \n";
-        cin >> fExt;
+                File.createFile(fName, fExt, fData);
+            }
+            else if (choice == 2){
+                cout << "Enter File Name \n";
+                cin >> fName;
+                cout << "Enter File extension \n";
+                cin >> fExt;
 
-        File.deleteFile(fName,fExt);
-    }
-    else if (choice == 3){
-        cout << "Enter File Name \n";
-        cin >> fName;
-        cout << "Enter File extention \n";
-        cin >> fExt;
-        File.viewFile(fName,fExt);
-    } else
-        cout << "Invalid choice!!\n"
-                "Try Again!!\n";
-
+                File.deleteFile(fName, fExt);
+            }
+            else if (choice == 3){
+                cout << "Enter File Name \n";
+                cin >> fName;
+                cout << "Enter File extention \n";
+                cin >> fExt;
+                File.viewFile(fName, fExt);
+            } else if(choice == 4){
+                break;
+            } else
+                cout << "Invalid choice!!\n"
+                        "Try Again!!\n";
+    } while (choice != 1 | choice != 2 || choice != 3 || choice != 4);
     return 0;
 }
